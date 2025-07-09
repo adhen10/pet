8096d577183e3a2f73dfb8361df20e6a9b82d071
Adhen Firman <adhen firman firdaus>
2025-07-09 21:20:27 +0700

    add lifecycle

--- app/Filament/Resources/ToolResource/Pages/CreateTool.php
+++ app/Filament/Resources/ToolResource/Pages/CreateTool.php
@@ -2,11 +2,45 @@
 
 namespace App\Filament\Resources\ToolResource\Pages;
 
-use App\Filament\Resources\ToolResource;
+use App\Filament\Resources\OwnerResource;
 use Filament\Actions;
+use App\Filament\Resources\ToolResource;
+use Filament\Notifications\Actions\Action;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateTool extends CreateRecord
 {
     protected static string $resource = ToolResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+
+    protected function getCreatedNotification(): ?Notification
+    {
+        return Notification::make()
+            ->success()
+            ->title('Tool Created');
+    }
+
+    protected function beforeCreate(): void
+    {
+        if (auth()->user()->id == 1) {
+            Notification::make()
+                ->warning()
+                ->title('You don\'t have an active subscription!')
+                ->body('Choose a plan to continue.')
+                ->persistent()
+                ->actions([
+                    Action::make('Subscribe')
+                        ->button()
+                        ->url(OwnerResource::getUrl('create'), shouldOpenInNewTab: true),
+                ])
+                ->send();
+                
+            $this->halt();
+        }
+    }
 }
